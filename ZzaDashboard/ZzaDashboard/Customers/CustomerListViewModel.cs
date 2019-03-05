a508e7e0c55cfce0e8b42840ff4537db53035ddb
Allay Desai <allaydesai@gmail.com>
2019-03-05 13:21:42 -0500

    Added Command for view to viewModel Communication

--- ZzaDashboard/ZzaDashboard/Customers/CustomerListViewModel.cs
+++ ZzaDashboard/ZzaDashboard/Customers/CustomerListViewModel.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Zza.Data;
 using ZzaDashboard.Services;
 
@@ -14,6 +17,8 @@ namespace ZzaDashboard.Customers
     {
         private ICustomersRepository _repo = new CustomersRepository();
         private ObservableCollection<Customer> _customers;
+        private Customer _selectedCustomer;
+        public RelayCommand DeleteCommand { get; private set; }
 
         public CustomerListViewModel()
         {
@@ -21,8 +26,32 @@ namespace ZzaDashboard.Customers
                 return;
             // Async function returns a Task.result() gives list => List using which construct a collection
             Customers = new ObservableCollection<Customer>(_repo.GetCustomersAsync().Result);
+            DeleteCommand = new RelayCommand(OnDelete, CanDelete);
+        }
+
+        private bool CanDelete()
+        {
+            return SelectedCustomer != null;
+        }
+
+        private void OnDelete()
+        {
+            Customers.Remove(SelectedCustomer);
         }
 
         public ObservableCollection<Customer> Customers { get => _customers; set => _customers = value; }
+
+        
+        public Customer SelectedCustomer {
+            get
+            {
+                return _selectedCustomer;
+            }
+            set
+            {
+                _selectedCustomer = value;
+                DeleteCommand.RaiseCanExecuteChanged();
+            }
+        }
     }
 }
